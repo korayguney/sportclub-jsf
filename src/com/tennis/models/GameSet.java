c83618f3b36ddd2f2392796045c0b3930c07f071
korayguney <korayguney@sabanciuniv.edu>
2020-08-20 21:35:59 +0300

    added GameSet

---
+++ src/com/tennis/models/GameSet.java
@@ -0,0 +1,65 @@
+package com.tennis.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class GameSet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int score1;
+	private int score2;
+	private int set_no;
+	
+	@OneToOne
+	private Game game;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getScore1() {
+		return score1;
+	}
+	public void setScore1(int score1) {
+		this.score1 = score1;
+	}
+	public int getScore2() {
+		return score2;
+	}
+	public void setScore2(int score2) {
+		this.score2 = score2;
+	}
+	public int getSet_no() {
+		return set_no;
+	}
+	public void setSet_no(int set_no) {
+		this.set_no = set_no;
+	}
+	
+	public Game getGame() {
+		return game;
+	}
+	public void setGame(Game game) {
+		this.game = game;
+	}
+	@Override
+	public String toString() {
+		return "GameSet [id=" + id + ", score1=" + score1 + ", score2=" + score2 + ", set_no=" + set_no + ", game="
+				+ game + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
